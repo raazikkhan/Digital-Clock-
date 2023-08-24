231b045d58a8ce67604edc26582c637cd015de08
Md Razik Khan <95939876+raazikkhan@users.noreply.github.com>
2023-08-24 20:59:20 +0530

    Add files via upload

---
+++ digital-clock.js
@@ -0,0 +1,10 @@
+let hours = document.getElementById("hrs");
+let mins = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+setInterval(()=>{
+let currentTime = new Date();
+hours.innerHTML = (currentTime.getHours()<10?"0":"")+ currentTime.getHours();
+mins.innerHTML = (currentTime.getMinutes()<10?"0":"")+ currentTime.getMinutes();
+sec.innerHTML = ( currentTime.getSeconds()<10?"0":"")+ currentTime.getSeconds();
+},1000)
